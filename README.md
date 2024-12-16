642b370f41d4c967c057749ec030369ef7ce6e4c
Ariabagus Mustikoatmodjo <ariabagus@Ariabaguss-MacBook-Air.local>
2024-12-16 19:38:49 +0700

    updates the readme and simulation file, and add a gitignore file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Ants-Movements-Simulation
+
+Creates a simulation of ants' movements during the search of food.
+
+This project is part of the exercise I did for the Mathematical Computation and Simulation course back at my university.
+
+# Execution
+
+Generate and visualize the simulations
+```
+python3 simulate.py
+```
--- simulate.py
+++ simulate.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import seaborn as sns
 
-
+# Initiate all the variables for the simulation
 maxpher = 50
 n = 17
 probAnt = 0.1
@@ -14,20 +14,19 @@ threshold = 50
 diffusionRate = 0.01
 iter = 50
 
-
+# Generates each iterations for the simulation 
 funcSemut = functions(maxpher, n, probAnt)
 list_matriks = np.zeros((iter, 17, 17))
 matriks = funcSemut.generate_matriks()
 posisi_semut = funcSemut.find_posisi_semut(matriks)
 
 list_matriks[0] = matriks
-
 for i in range(1, iter):
     matriks, posisi_semut = funcSemut.pergerakan_semut(matriks, deposit, posisi_semut)
     matriks = funcSemut.difusi_feromone(diffusionRate, matriks, evaporate)
     list_matriks[i] = matriks
 
-
+# Visualize the simulation
 def init():
     plt.clf()
     return None
@@ -41,12 +40,10 @@ def animate(i):
     return None
 
 fig = plt.figure()
-
 anim = animation.FuncAnimation(fig,
                                animate,
                                frames=range(0, iter, 1),
                                blit=False,
                                interval=50,
                                init_func=init)  
-
 plt.show()
